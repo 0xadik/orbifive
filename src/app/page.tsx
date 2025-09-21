2734361072dbfd515230ae72c8790187330d89c5
Md Sadikul Islam <sadikulislamofficial7@hotmail.com>
2025-09-22 00:43:34 +0600

    new

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,8 +12,8 @@ import Video from "@/components/Video";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Free Next.js Template for Startup and SaaS",
-  description: "This is Home for Startup Nextjs Template",
+  title: "Orbifive - E-Commerce Development & Management Service  ",
+  description: "This is Home for E-Commerce Startup",
   // other metadata
 };
 
